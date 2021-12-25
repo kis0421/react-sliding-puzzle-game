9a88a9c1ec9ee84ec8130e06fc0067a257131f06
김민규 <kig0421@gmail.com>
2021-12-25 18:56:59 +0900

    Tile, NavigationHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import * as React from "react";
+import NavigationHeader from "./components/NavigationHeader";
+import Tile from "./components/Tile";
 
 const App = () => {
-  return <>hello</>;
+  return <div id="slidingPuzzleWrap">
+    <NavigationHeader />
+    <div className="puzzleRowWrap">
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+    </div>
+
+  </div>;
 }
 export default App;
