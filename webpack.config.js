5ee2735d9b13858cf76a1565361ff40ef7062da3
김민규 <kig0421@gmail.com>
2021-12-25 17:34:47 +0900

    initialize files

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ["babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({ template: "./src/index.html" }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+}
