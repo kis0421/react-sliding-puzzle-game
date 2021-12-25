ec2a37cc76f7017a446411e8cbbe60a413a81c44
김민규 <kig0421@gmail.com>
2021-12-25 20:19:39 +0900

    array division 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
 export function shuffle<T>(array: T[]) {
   return array.sort(() => Math.random() > 0.5 ? 1 : -1);
 }
+
+export function divisionArray<T>(array: T[], unit: number) {
+  const unitCount = Math.max(unit, 1);
+  const tempArray = [];
+  for (let i = 0; i < Math.ceil(array.length / unitCount); i++) {
+    tempArray[i] = array.slice(i * unitCount, (i * unitCount) + unitCount)
+  }
+  return tempArray;
+}
