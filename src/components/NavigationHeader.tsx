416d77614a49f064469abe7c1dd7a3e09cbd65a2
김민규 <kig0421@gmail.com>
2021-12-25 18:57:13 +0900

    Header Navagation 추가

---
+++ src/components/NavigationHeader.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const NavigationHeader = () => {
+  return (
+    <header style={{ height: "100px" }}>
+      todo
+    </header>);
+}
+
+export default NavigationHeader;
