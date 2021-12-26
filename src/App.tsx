96b252c5234f698a46e1ca4d7e785c25bc0fa64a
kis0421 <kig0421@gmail.com>
2021-12-26 19:16:01 +0900

    테스트 코드 및 return 심플하게

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,17 @@ import { shuffle, divisionArray } from "./utils"
 
 const App = () => {
   const row = 4;
-  const defaultTiles = divisionArray(shuffle([0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15]), row)
+  const defaultTiles = divisionArray(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]), row)
   const [tiles, setTiles] = useState(defaultTiles)
 
   return <div id="slidingPuzzleWrap">
     <NavigationHeader />
-    {tiles.map((puzzleWrap, index) => {
-      return <div className="puzzleRowWrap" key={index}>
+    {tiles.map((puzzleWrap, index) =>
+      <div className="puzzleRowWrap" key={index}>
         {puzzleWrap.map((tile) => {
           return <Tile tileNumber={tile} key={tile} />
         })}
-      </div>
-    })}
+      </div>)}
   </div>;
 }
 export default App;
