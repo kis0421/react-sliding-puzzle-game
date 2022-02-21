c08e44ef1ec0d289b19b2826656acc578bcbfcaa
김민규 <kig0421@gmail.com>
2022-02-21 20:06:48 +0900

    인접한 타일만 위치 바꿀 수 있게 로직 구현

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,9 +2,29 @@ import * as React from "react";
 import classnames from "classnames";
 
 const Tile = (props: { tileNumber: number, tiles: number[][], setTiles: React.Dispatch<React.SetStateAction<number[][]>> }) => {
-  const { tileNumber } = props;
+  const { tileNumber, tiles, setTiles } = props;
+  const changeTile = () => {
+    const currentRowIndex = tiles.findIndex((i) => i.includes(tileNumber));
+    const currentColumnIndex = tiles[currentRowIndex].findIndex((i) => i === tileNumber);
+    const currentIndexes = [currentRowIndex, currentColumnIndex];
 
-  return <div className={classnames("tile", { "tile-hide": tileNumber === 0 })} >
+    const zeroRowIndex = tiles.findIndex((i) => i.includes(0));
+    const zeroColumnIndex = tiles[zeroRowIndex].findIndex((i) => !i);
+    const zeroIndexes = [zeroRowIndex, zeroColumnIndex];
+
+    if ((!Math.abs(currentIndexes[0] - zeroIndexes[0]) && Math.abs(currentIndexes[1] - zeroIndexes[1]) === 1)
+      || (Math.abs(currentIndexes[0] - zeroIndexes[0]) === 1 && !Math.abs(currentIndexes[1] - zeroIndexes[1]))) {
+      const newTiles = [...tiles];
+      newTiles[currentIndexes[0]][currentIndexes[1]] = 0;
+      newTiles[zeroIndexes[0]][zeroIndexes[1]] = tileNumber;
+      setTiles(newTiles)
+    }
+
+
+  }
+  return <div
+    onClick={changeTile}
+    className={classnames("tile", { "tile-hide": tileNumber === 0 })} >
     <span>{tileNumber}</span>
   </div>
 }
