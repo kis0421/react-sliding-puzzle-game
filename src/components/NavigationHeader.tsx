cb233007a27ddf038c4f0d0ba6eb90a2ddf1962d
kis0421 <kig0421@gmail.com>
2022-02-22 21:05:04 +0900

    header movecount UI

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -4,7 +4,7 @@ const NavigationHeader = (props: { moveCount: number }) => {
   const { moveCount } = props;
   return (
     <header style={{ height: "100px" }}>
-      todo {moveCount}
+      <div style={{ float: "right", margin: "4px 12px" }}>움직인 횟수: {moveCount}</div>
     </header>);
 }
 
