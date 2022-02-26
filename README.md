b4c2699cb0e668b0f0afdec327b8fb34ecd73741
kis0421 <kig0421@gmail.com>
2022-02-27 07:50:39 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# react-sliding-puzzle
-
-TODO SOON.
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
-import "./css/index.css"
-
-import App from "./App";
-
-ReactDom.render(<App />, document.getElementById("app"))
