345e02d584a35c078585209eeeeb938025b9db00
김민규 <kig0421@gmail.com>
2022-03-16 12:17:21 +0900

    타일 이동 방향moveDirection 추가

--- src/utils.ts
+++ src/utils.ts
@@ -11,7 +11,7 @@ export function divisionArray<T>(array: T[], unit: number) {
   return tempArray;
 }
 
-export function getAfterChangeTiles(tiles: number[][], tileNumber: number) {
+export function getAfterChangeTiles(tiles: number[][], tileNumber: number): [number[][], "left" | "top" | "right" | "bottom"] | undefined {
   const currentRowIndex = tiles.findIndex((i) => i.includes(tileNumber));
   const currentColumnIndex = tiles[currentRowIndex].findIndex((i) => i === tileNumber);
   const currentIndexes = [currentRowIndex, currentColumnIndex];
@@ -20,12 +20,20 @@ export function getAfterChangeTiles(tiles: number[][], tileNumber: number) {
   const zeroColumnIndex = tiles[zeroRowIndex].findIndex((i) => !i);
   const zeroIndexes = [zeroRowIndex, zeroColumnIndex];
 
-  if ((!Math.abs(currentIndexes[0] - zeroIndexes[0]) && Math.abs(currentIndexes[1] - zeroIndexes[1]) === 1)
-    || (Math.abs(currentIndexes[0] - zeroIndexes[0]) === 1 && !Math.abs(currentIndexes[1] - zeroIndexes[1]))) {
+  const moveDirection = currentIndexes[0] - zeroIndexes[0] === 1 && currentIndexes[1] - zeroIndexes[1] === 0
+    ? "top"
+    : currentIndexes[0] - zeroIndexes[0] === -1 && currentIndexes[1] - zeroIndexes[1] === 0
+      ? "bottom"
+      : currentIndexes[0] - zeroIndexes[0] === 0 && currentIndexes[1] - zeroIndexes[1] === 1
+        ? "left"
+        : currentIndexes[0] - zeroIndexes[0] === 0 && currentIndexes[1] - zeroIndexes[1] === -1
+          ? "right"
+          : null;
+
+  if (moveDirection) {
     const newTiles = [...tiles];
     newTiles[currentIndexes[0]][currentIndexes[1]] = 0;
     newTiles[zeroIndexes[0]][zeroIndexes[1]] = tileNumber;
-
-    return newTiles;
+    return [newTiles, moveDirection]
   }
 }
