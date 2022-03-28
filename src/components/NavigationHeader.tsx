deef518719d2ca1ad60f1ff9d4354568957f09bb
kis0421 <kig0421@gmail.com>
2022-03-28 20:49:58 +0900

    inline style css파일로 옮기기

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -3,9 +3,9 @@ import * as React from "react";
 const NavigationHeader = (props: { moveCount: number, isCompletePuzzle: boolean }) => {
   const { moveCount, isCompletePuzzle } = props;
   return (
-    <header style={{ height: "100px" }}>
-      <div style={{ float: "right", margin: "4px 12px" }}>움직인 횟수: {moveCount}</div>
-      {isCompletePuzzle && <div style={{ clear: "both", float: "right", margin: "4px 12px", color: "green" }}>성공</div>}
+    <header className="gameBoardHeader" >
+      <div>움직인 횟수: {moveCount}</div>
+      {isCompletePuzzle && <div>성공</div>}
     </header>);
 }
 
